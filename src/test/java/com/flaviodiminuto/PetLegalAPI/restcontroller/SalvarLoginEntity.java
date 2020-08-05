e0fc385e1acdd1efa966d70aee58aba2a778178d
flaviodiminuto <flavio.diminuto@outlook.com>
2020-08-05 00:43:12 -0300

    Ajuste mensagems de resposta

--- src/test/java/com/flaviodiminuto/PetLegalAPI/restcontroller/SalvarLoginEntity.java
+++ src/test/java/com/flaviodiminuto/PetLegalAPI/restcontroller/SalvarLoginEntity.java
@@ -1,39 +1,76 @@
 package com.flaviodiminuto.PetLegalAPI.restcontroller;
 
+import com.flaviodiminuto.PetLegalAPI.enumeradores.Mensagem;
 import com.flaviodiminuto.PetLegalAPI.model.build.LoginBuilder;
 import com.flaviodiminuto.PetLegalAPI.model.entity.LoginEntity;
-import com.flaviodiminuto.PetLegalAPI.usecase.interfaces.SaveUsecase;
+import com.flaviodiminuto.PetLegalAPI.restcontroller.post.LoginPost;
 import io.restassured.http.ContentType;
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 
 import java.time.LocalDateTime;
 import java.util.UUID;
 
-import static io.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.*;
-//import static io.restassured.module.mockmvc.RestAssuredMockMvc.*;
-//import static io.restassured.module.mockmvc.matcher.RestAssuredMockMvcMatchers.*;
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
 
 public class SalvarLoginEntity {
     private final String recurso = "/credencial";
+    private static final LoginEntity entity = setup();
+    private final String campoMensagem = "mensagem";
 
-    @Test
-    public void testCadastroCredencial(){
+
+    public static LoginEntity setup(){
         String identificador = UUID.randomUUID().toString();
         String senha = "senha_teste";
-
-        LoginEntity loginEntity = new LoginBuilder().identificador(identificador)
+        return  new LoginBuilder().identificador(identificador)
                 .senha(senha)
                 .dataCriacao(LocalDateTime.now())
                 .build();
+    }
+
+    @Test
+    public void testCadastroCredencial200(){
+        setup();
         given().
-                body(loginEntity).
+                body(entity).
                 contentType(ContentType.JSON).
         when().
                 post(recurso).
         then()
+                .assertThat()
+                .contentType(ContentType.JSON)
+                .body(campoMensagem, equalTo(Mensagem.SALVO_COM_SUCESSO))
                 .statusCode(201);
+    }
+
+    @Test
+    public void testCadastroIdentificadorExistente406(){
+        given().
+                body(entity).
+                contentType(ContentType.JSON).
+        when().
+                post(recurso).
+        then().
+                assertThat().
+                body(campoMensagem, equalTo(Mensagem.IDENTIFICADOR_INDISPONIVEL)).
+                contentType(ContentType.JSON).
+                statusCode(406);
+    }
+
+    @Test
+    public void testCadastroException500(){
+        RestAssuredMockMvc.given().
+                standaloneSetup(LoginPost.class).
+                body(entity).
+                contentType(ContentType.JSON).
+        when().
+                post(recurso).
+        then().
+                assertThat().
+                contentType(ContentType.JSON).
+                body(campoMensagem, equalTo(Mensagem.INTERNAL_ERROR)).
+                statusCode(500);
     }
 
 }
