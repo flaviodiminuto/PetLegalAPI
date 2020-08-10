b1e82fdbf40deb97f8206b9c96118089888bd7f7
flaviodiminuto <flaviodiminuto@outlook.com>
2020-08-10 13:16:45 -0300

    Ajuste do Enumerador de mensagens

--- src/main/java/com/flaviodiminuto/PetLegalAPI/enumeradores/Mensagem.java
+++ src/main/java/com/flaviodiminuto/PetLegalAPI/enumeradores/Mensagem.java
@@ -1,10 +1,9 @@
 package com.flaviodiminuto.PetLegalAPI.enumeradores;
 
 public enum Mensagem {
-    //todo - transformar em enumerador =)
-    INTERNAL_ERROR ("Ocorreu um erro inexperado, tente novamente mais tarde")
-    ,IDENTIFICADOR_INDISPONIVEL ("Identificador indisponivel,por favor tente outro")
-    ,SALVO_COM_SUCESSO ("Salvo com sucesso!");
+    INTERNAL_ERROR ("Ocorreu um erro inexperado, tente novamente mais tarde"),
+    IDENTIFICADOR_INDISPONIVEL ("Identificador indisponivel,por favor tente outro"),
+    SALVO_COM_SUCESSO ("Salvo com sucesso!");
 
     private String message;
     Mensagem(String message) {
